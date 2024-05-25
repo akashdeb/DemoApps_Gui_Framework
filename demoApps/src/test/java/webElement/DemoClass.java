1a9ae71397b27c20cf576466e75cb2119f88d914
akashdeb <akashdeb87@gmail.com>
2024-05-25 13:35:00 +0530

    Now I will get conflict

--- demoApps/src/test/java/webElement/DemoClass.java
+++ demoApps/src/test/java/webElement/DemoClass.java
@@ -5,6 +5,8 @@ public class DemoClass {
 	public static void main(String[] args) {
 		
 		System.out.println("This are the new Changes");
+		
+		System.out.println("The changes after Vacation");
 
 	}
 
